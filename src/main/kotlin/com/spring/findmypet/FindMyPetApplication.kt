ab55ea96fc478ab1d8e5e4aac2fbc06a2e971ecf
filip <filip.nikolic@kupujemprodajem.com>
2025-02-22 11:33:41 +0100

    Initial commit

---
+++ src/main/kotlin/com/spring/findmypet/FindMyPetApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.findmypet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FindMyPetApplication
+
+fun main(args: Array<String>) {
+	runApplication<FindMyPetApplication>(*args)
+}
---
+++ src/test/kotlin/com/spring/findmypet/FindMyPetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spring.findmypet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FindMyPetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
